8b02e8e50aa116d5be7df304ef1b139fdddfc6f9
Cathy Oun <cathy810218@gmail.com>
2017-05-01 16:28:27 -0700

    Add keys.h in gitignore and configure Parse

---
+++ LocationReminders/Keys.h
@@ -0,0 +1,17 @@
+//
+//  Keys.h
+//  LocationReminders
+//
+//  Created by Cathy Oun on 5/1/17.
+//  Copyright © 2017 cathyoun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Keys : NSObject
+
+extern NSString *const kAppID;
+extern NSString *const kClientKey;
+extern NSString *const kServerURL;
+
+@end
